87a7b85ae27350a1e8165c814c0598c3495e1f91
Filip Markovic <f12markovic@gmail.com>
2021-04-25 00:01:40 +0200

    Add zsh termtitle

---
+++ .config/zsh/plugins/termtitle.zsh
@@ -0,0 +1,14 @@
+autoload -Uz add-zsh-hook
+
+function title_precmd () {
+	print -Pn -- '\e]2;%n@%m %~\a'
+	[[ "$TERM" == 'screen'* ]] && print -Pn -- '\e_\005{g}%n\005{-}@\005{m}%m\005{-} \005{B}%~\005{-}\e\\'
+}
+
+function title_preexec () {
+	print -Pn -- '\e]2;%n@%m %~ %# ' && print -n -- "${(q)1}\a"
+	[[ "$TERM" == 'screen'* ]] && { print -Pn -- '\e_\005{g}%n\005{-}@\005{m}%m\005{-} \005{B}%~\005{-} %# ' && print -n -- "${(q)1}\e\\"; }
+}
+
+add-zsh-hook -Uz precmd title_precmd
+add-zsh-hook -Uz preexec title_preexec
